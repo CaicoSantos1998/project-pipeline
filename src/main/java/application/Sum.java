53b6aa7442ba49fba43846ea95521f9a8e8de9c4
CS <caico_fc@hotmail.com>
2026-02-20 16:41:01 -0300

    First commit

---
+++ src/main/java/application/Sum.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Sum {
+
+	public double sum(double n1, double n2) {
+		return n1 + n2;
+	}
+}
---
+++ src/test/java/teste/SumTest.java
@@ -0,0 +1,16 @@
+package teste;
+
+import application.Sum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumTest {
+	
+	@Test
+	public void sumTest() {
+		Sum account = new Sum();
+		double result = account.sum(10.0, 5.0);
+		assertEquals(15.0, result, 0);
+	}
+}
